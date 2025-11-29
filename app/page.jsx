884db5c4f78f721441b6b83356e74caf3f1fb79f
Lewis <ljelfswebdev@gmail.com>
2025-11-29 17:56:56 +0000

    update for server routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,38 +1,37 @@
-import dynamic from 'next/dynamic';
+import NextDynamic from 'next/dynamic';
+export const dynamic = 'force-dynamic';
 import { dbConnect } from '@helpers/db';
 import Page from '@/models/Page';
 import HeroSection from '@/components/Homepage/HeroSection';
-
-// Lazy sections
-const GallerySection = dynamic(
+const GallerySection = NextDynamic(
   () => import('@/components/Homepage/GallerySection'),
   { ssr: false }
 );
-const AboutSection = dynamic(
+const AboutSection = NextDynamic(
   () => import('@/components/Homepage/AboutSection'),
   { ssr: false }
 );
-const HouseRulesSection = dynamic(
+const HouseRulesSection = NextDynamic(
   () => import('@/components/Homepage/HouseRulesSection'),
   { ssr: false }
 );
-const AmenitiesSection = dynamic(
+const AmenitiesSection = NextDynamic(
   () => import('@/components/Homepage/AmenitiesSection'),
   { ssr: false }
 );
-const PlacesToEatSection = dynamic(
+const PlacesToEatSection = NextDynamic(
   () => import('@/components/Homepage/PlacesToEatSection'),
   { ssr: false }
 );
-const TouristAttractionsSection = dynamic(
+const TouristAttractionsSection = NextDynamic(
   () => import('@/components/Homepage/TouristAttractionsSection'),
   { ssr: false }
 );
-const ContactSection = dynamic(
+const ContactSection = NextDynamic(
   () => import('@/components/Homepage/ContactSection'),
   { ssr: false }
 );
-const FaqSection = dynamic(
+const FaqSection = NextDynamic(
   () => import('@/components/Homepage/FaqSection'),
   { ssr: false }
 );
@@ -58,7 +57,6 @@ export default async function HomePage() {
   return (
     <>
       <HeroSection data={data.section1 || {}} />
-
       <GallerySection data={data.section2 || {}} />
       <AboutSection data={data.section3 || {}} />
       <HouseRulesSection data={data.section4 || {}} />
